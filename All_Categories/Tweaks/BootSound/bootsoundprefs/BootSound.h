454c4cbdd74c8397189ff11796cf7599b918777e
EmokidxD <46846645+EmokidxD@users.noreply.github.com>
2023-02-10 14:36:25 -0500

    update

--- All_Categories/Tweaks/BootSound/bootsoundprefs/BootSound.h
+++
@@ -1,8 +0,0 @@
-#import <Preferences/PSListController.h>
-#include <CepheiPrefs/HBRootListController.h>
-#include <CepheiPrefs/HBAppearanceSettings.h>
-#include "HBRespringController.h"
-
-@interface BootSound : HBListController
--(void)Respring;
-@end
